bcb19f108b3349cfcd44f6b943fa8412b0024f69
travisp <paul.travis@michigansugar.com>
2024-06-16 10:01:21 -0400

    Updated format.sh now that thumbs is unused

--- upload/format.sh
+++ upload/format.sh
@@ -21,9 +21,9 @@ count=(`find ./ -maxdepth 1 -name "*.jpg"`)
 if [ ${#count[@]} -gt 0 ]; then
 
   rename 's/img/IMG/' ./*.jpg
-	cp ./*.jpg ../img/ 
-	mogrify -resize 250x250 *.jpg 
-	mv ./*.jpg ../thumbs/
+	mv ./*.jpg ../img/ 
+	#mogrify -resize 250x250 *.jpg 
+	#mv ./*.jpg ../thumbs/
 
 fi
 
@@ -40,14 +40,14 @@ if [ ${#count[@]} -gt 0 ]; then
 
   rename 's/VID/IMG/' ./*.mp4
 
-	for i in *.mp4; do
-	    [ -f "$i" ] || break
-	    j=${i%.*}
-	    ffmpeg -i $i -ss 00:00:01.000 -vframes 1 "$j.png" > /dev/null 2> /dev/null
-	done
+	#for i in *.mp4; do
+	#    [ -f "$i" ] || break
+	#    j=${i%.*}
+	#    ffmpeg -i $i -ss 00:00:01.000 -vframes 1 "$j.png" > /dev/null 2> /dev/null
+	#done
 
-	mogrify -resize 250x250 *.png
-	mv ./*.png ../thumbs/
+	#mogrify -resize 250x250 *.png
+	#mv ./*.png ../thumbs/
 	mv ./*.mp4 ../img/
 
 fi
